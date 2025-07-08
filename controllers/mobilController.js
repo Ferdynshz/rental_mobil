c7a36647bb2915d4c6a2bb102663b6eeab31253f
adhaa2 <rahmaasariadha@gmail.com>
2025-07-08 10:25:01 +0700

    aa

--- controllers/mobilController.js
+++ controllers/mobilController.js
@@ -18,14 +18,18 @@ exports.createForm = (req, res) => {
 };
 
 exports.create = (req, res) => {
-    const newMobil = {
-        id: mobils.length + 1,
-        merk: req.body.merk,
-        tahun: req.body.tahun
-    };
-    mobils.push(newMobil);
-    res.redirect('/mobil');
+  const newMobil = {
+    id: mobils.length + 1,
+    merk: req.body.merk,
+    tahun: req.body.tahun,
+    spesifikasi: req.body.spesifikasi,
+    nomor_polisi: req.body.nomor_polisi,
+    harga_sewa: parseInt(req.body.harga_sewa)
+  };
+  mobils.push(newMobil);
+  res.redirect('/mobil');
 };
+
 
 exports.editForm = (req, res) => {
     const id = parseInt(req.params.id);
@@ -36,10 +40,20 @@ exports.editForm = (req, res) => {
 exports.update = (req, res) => {
     const id = parseInt(req.params.id);
     const mobil = mobils.find(m => m.id === id);
+
+    if (!mobil) {
+        return res.status(404).send("Mobil tidak ditemukan.");
+    }
+
     mobil.merk = req.body.merk;
-    mobil.tahun = req.body.tahun;
+    mobil.spesifikasi = req.body.spesifikasi;
+    mobil.tahun = parseInt(req.body.tahun);
+    mobil.nomor_polisi = req.body.nomor_polisi;
+    mobil.harga_sewa = parseInt(req.body.harga_sewa);
+
     res.redirect('/mobil');
 };
+
 
 exports.delete = (req, res) => {
     mobils = mobils.filter(m => m.id !== parseInt(req.params.id));
